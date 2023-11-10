d299738319a2e6d64804d321cb3f586e9bfc359d
ashwithans <ashwithans@gmail.com>
2023-11-10 15:44:16 +0530

    Assignment_Day05

---
+++ ConsAppDay5Assignment1/ConsAppDay5Assignment1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppDay5Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[][] matrix = new int[3][];
+            //matrix[0] = new int[4] { 1, 2, 3, 4 };
+            //matrix[1] = new int[2] { 5, 6 };
+            //matrix[2] = new int[3] { 7, 8, 9 };
+            //Console.WriteLine("Matrix[0][0]: " + matrix[0][0]);
+            //Console.WriteLine("Matrix[1][1]: " + matrix[1][1]);
+
+            //foreach (int[] row in matrix)
+            //{
+            //    foreach (int element in row)
+            //    {
+            //        Console.Write(element + " ");
+            //    }
+            //    Console.WriteLine();
+
+
+
+            //2nd task
+            //int element = matrix[0][1];
+
+            //foreach (int[] row in matrix)
+            //{
+            //    foreach (int item in row)
+            //    {
+            //        Console.Write(item + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //    Console.WriteLine("Value of the second element in the first row (element): " + element);
+
+
+
+            //    task3
+            //int[][] matrix = new int[3][];
+            //matrix[0] = new int[4] { 1, 2, 3, 4 };
+            //matrix[1] = new int[3] { 5, 6, 0 };
+            //matrix[2] = new int[3] { 7, 8, 9 };
+            //matrix[1][2] = 15;
+
+
+            //foreach (int[] row in matrix)
+            //{
+            //    foreach (int item in row)
+            //    {
+            //        Console.Write(item + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //    task4
+            object[][] objectMatrix = new object[2][];
+            objectMatrix[0] = new object[] { 42, "Hello", 3.14 };
+            objectMatrix[1] = new object[] { true, 'A', DateTime.Now };
+
+
+            foreach (object[] row in objectMatrix)
+            {
+                foreach (object item in row)
+                {
+                    Console.WriteLine($"Type: {item.GetType()}, Value: {item}");
+                }
+
+            }
+        }
+        }
+    }
+
+    
+
+
---
+++ ConsAppDay5Assignment2/ConsAppDay5Assignment2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsAppDay5Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList studentGrades = new ArrayList();
+            studentGrades.Add(85);
+            studentGrades.Add(92);
+            studentGrades.Add(78);
+            studentGrades.Add(95);
+            studentGrades.Add(88);
+
+
+            //task2
+            Console.WriteLine("Displaying the contents of studentGrades:");
+            foreach (int grade in studentGrades)
+            {
+                Console.WriteLine(grade);
+            }
+
+            int sum = 0;
+            foreach (int grade in studentGrades)
+            {
+                sum += grade;
+            }
+
+            Console.WriteLine("The sum of all grades in studentGrades: " + sum);
+            Console.WriteLine();
+            
+
+            //task3
+            Console.WriteLine("Adding a new grade of 90 to the end of studentGrades.");
+            studentGrades.Add(90); // Add a new grade of 90
+
+            Console.WriteLine(" Displaying the updated contents of studentGrades:");
+            foreach (int grade in studentGrades)
+            {
+                Console.WriteLine(grade);
+            }
+            Console.WriteLine();
+
+
+
+            //task4
+            Console.WriteLine("Removing the grade of 78 from studentGrades.");
+            studentGrades.Remove(78); // Remove the grade of 78
+
+            Console.WriteLine("Displaying the updated contents of studentGrades:");
+            foreach (int grade in studentGrades)
+            {
+                Console.WriteLine(grade);
+            }
+            Console.WriteLine();
+
+
+            //task5
+            Console.WriteLine("Searching for the index of the grade 95 in studentGrades.");
+            int index = studentGrades.IndexOf(95);
+
+            if (index != -1)
+            {
+                Console.WriteLine("Found the grade 95 at index " + index);
+                Console.WriteLine("Updating the grade at index " + index + " to 96.");
+                studentGrades[index] = 96; // Update the grade at the found index to 96
+            }
+            else
+            {
+                Console.WriteLine("Grade 95 not found in studentGrades.");
+            }
+
+            Console.WriteLine("Displaying the updated contents of studentGrades:");
+            foreach (int grade in studentGrades)
+            {
+                Console.WriteLine(grade);
+            }
+            Console.ReadKey();
+        }
+    }
+}
